f2c4099ba1bd6febdad23294968a548130db5571
Tage Johansson <tajo9185@student.uu.se>
2024-08-30 01:41:10 +0200

    web-leptos: A minimal, not working, app.

--- web-leptos/src/core.rs
+++ web-leptos/src/core.rs
@@ -1,24 +1,66 @@
+#![allow(unused_variables, dead_code)]
 use std::rc::Rc;
 
+use crux_geolocation::{GeoOptions, GeoRequest};
 use leptos::{SignalUpdate, WriteSignal};
-use shared::{Counter, Effect, Event, ViewModel};
+use leptos_use::{use_geolocation_with_options, UseGeolocationOptions, UseGeolocationReturn};
+use shared::{view_types::ViewModel, Effect, Event, GeoApp, Request};
 
-pub type Core = Rc<shared::Core<Effect, Counter>>;
+/// The app type that will be passed around.
+pub type App = Rc<AppStruct>;
 
-pub fn new() -> Core {
-    Rc::new(shared::Core::new())
+/// Core struct with information about the app.
+pub struct AppStruct {
+    /// The core of the app.
+    pub core: shared::Core<Effect, GeoApp>,
 }
 
-pub fn update(core: &Core, event: Event, render: WriteSignal<ViewModel>) {
-    for effect in core.process_event(event) {
-        process_effect(core, effect, render);
+pub fn new_app() -> App {
+    Rc::new(AppStruct {
+        core: shared::Core::new(),
+    })
+}
+
+pub fn update(app: &App, event: Event, render: WriteSignal<ViewModel>) {
+    for effect in app.core.process_event(event) {
+        process_effect(app, effect, render);
     }
 }
 
-pub fn process_effect(core: &Core, effect: Effect, render: WriteSignal<ViewModel>) {
+pub fn process_effect(app: &App, effect: Effect, render: WriteSignal<ViewModel>) {
     match effect {
         Effect::Render(_) => {
-            render.update(|view| *view = core.view());
+            render.update(|view| *view = app.core.view());
         }
+        Effect::Geolocation(req) => process_geolocation(app, req),
     };
+}
+
+fn process_geolocation(app: &App, request: Request<GeoRequest>) {
+    match &request.operation {
+        GeoRequest::GetCurrentPosition(opts) => {
+            todo!();
+        }
+        GeoRequest::WatchPosition(opts) => {
+            todo!();
+        }
+        GeoRequest::ClearWatch(_) => {
+            todo!();
+        }
+    }
+}
+
+/// Convert a `GeoOptions` struct from `crux_geolocation` to a similar "options struct" used by
+/// `leptos_use`.
+fn convert_geo_options(opts: GeoOptions) -> UseGeolocationOptions {
+    UseGeolocationOptions::default()
+        .immediate(false)
+        .enable_high_accuracy(opts.enable_high_accuracy)
+        .maximum_age(opts.maximum_age.try_into().unwrap_or(u32::MAX))
+        .timeout(
+            opts.timeout
+                .unwrap_or(u64::MAX)
+                .try_into()
+                .unwrap_or(u32::MAX),
+        )
 }
--- web-leptos/src/main.rs
+++ web-leptos/src/main.rs
@@ -1,44 +1,104 @@
+#![allow(unused_imports)]
 mod core;
 
-use leptos::{component, create_effect, create_signal, view, IntoView, SignalGet, SignalUpdate};
-use shared::Event;
+use std::fmt;
+use std::hash::{DefaultHasher, Hash};
+
+use leptos::{component, create_effect, create_node_ref, ev, html, signal_prelude::*, IntoView};
+use shared::{view_types::ViewModel, Event};
 
 #[component]
 fn RootComponent() -> impl IntoView {
-    let core = core::new();
-    let (view, render) = create_signal(core.view());
-    let (event, set_event) = create_signal(Event::Reset);
+    let app = core::new_app();
+    let (view, render) = create_signal(app.core.view());
+    let (event, set_event) = create_signal(Event::StartGeolocation);
 
     create_effect(move |_| {
-        core::update(&core, event.get(), render);
+        core::update(&app, event.get(), render);
     });
 
-    view! {
-        <section class="box container has-text-centered m-5">
-            <p class="is-size-5">{move || view.get().count}</p>
-            <div class="buttons section is-centered">
-                <button class="button is-primary is-danger"
-                    on:click=move |_| set_event.update(|value| *value = Event::Reset)
-                >
-                    {"Reset"}
-                </button>
-                <button class="button is-primary is-success"
-                    on:click=move |_| set_event.update(|value| *value = Event::Increment)
-                >
-                    {"Increment"}
-                </button>
-                <button class="button is-primary is-warning"
-                    on:click=move |_| set_event.update(|value| *value = Event::Decrement)
-                >
-                    {"Decrement"}
-                </button>
-            </div>
-        </section>
+    html::div().child((
+        curr_pos_component(view),
+        list_saved_positions(view),
+        save_pos_component(set_event),
+    ))
+}
+
+fn curr_pos_component(view: ReadSignal<ViewModel>) -> impl IntoView {
+    html::div().child((
+        html::h3().child("Current Position"),
+        html::p().child(move || {
+            let view = view.get();
+            (
+                format!("status: {}", view.gps_status),
+                view.curr_pos.as_ref().map(|pos| {
+                    (
+                        html::br(),
+                        format!("{}", pos.latitude),
+                        html::br(),
+                        format!("{}", pos.longitude),
+                        pos.altitude.as_ref().map(|x| (html::br(), format!("{x}"))),
+                    )
+                }),
+            )
+        }),
+    ))
+}
+
+fn list_saved_positions(view: ReadSignal<ViewModel>) -> impl IntoView {
+    html::div().child((
+        html::h3().child("Near Positions"),
+        html::p().child(move || format!("{} saved positions", view.get().near_positions.len())),
+        leptos::For(leptos::ForProps {
+            each: move || view.get().near_positions,
+            key: |x| x.hash(&mut DefaultHasher::new()),
+            children: |x| {
+                format!(
+                    "{}: {}, {}, {}",
+                    x.name, x.latitude, x.longitude, x.timestamp
+                )
+            },
+        }),
+    ))
+}
+
+fn save_pos_component(set_event: WriteSignal<Event>) -> impl IntoView {
+    let (save_pos_dialog, set_save_pos_dialog) = create_signal(false);
+    let input_node = create_node_ref();
+    move || {
+        if save_pos_dialog.get() {
+            html::form()
+                .child(
+                    html::label()
+                        .attr("for", "name")
+                        .child("Name of the position"),
+                )
+                .child(
+                    html::input()
+                        .attr("type", "text")
+                        .attr("name", "name")
+                        .node_ref(input_node),
+                )
+                .child(html::input().attr("type", "submit").attr("value", "Submit"))
+                .on(ev::submit, move |event| {
+                    event.prevent_default();
+                    let name = input_node
+                        .get()
+                        .expect("Input element should be initialized.")
+                        .value();
+                    set_event.set(Event::SaveCurrPos(name.into()));
+                    set_save_pos_dialog.set(false);
+                })
+                .into_any()
+        } else {
+            html::button()
+                .on(ev::click, move |_| set_save_pos_dialog.set(true))
+                .child("Save the Current Position ")
+                .into_any()
+        }
     }
 }
 
 fn main() {
-    leptos::mount_to_body(|| {
-        view! { <RootComponent /> }
-    });
+    leptos::mount_to_body(|| RootComponent());
 }
